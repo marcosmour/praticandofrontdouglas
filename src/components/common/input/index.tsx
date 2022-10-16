6fe3fb95512604af1d66897f0b1f7d247589c153
Marcos Moura <marcosmoura201@hotmail.com>
2022-10-16 17:40:45 -0300

    criando o axios para comunicar o backend com o frontend

--- src/components/common/input/index.tsx
+++ src/components/common/input/index.tsx
@@ -1,4 +1,6 @@
-interface InputProps{
+import {InputHTMLAttributes} from 'react'       ///
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+    id: string;
     onChange?: (value:any) => void;
     label: string;
     columnClasses?: string;
@@ -9,20 +11,21 @@ interface InputProps{
 export const Input: React.FC<InputProps> = ({
     onChange,
     label,
-    columnClasses
+    columnClasses,
+    id,
+    ...inputProps
 }:InputProps) => {
     return (
         <div className={` field column ${columnClasses}`}>
-            <label className="label" htmlFor="inputSku">{label}</label>
+            <label className="label" htmlFor={id}>{label}</label>
             <div className="control">
                 <input className="input"
-                    id="inputSku" value={""}
+                    id={id} {...inputProps}
                     onChange={event => {
                         if(onChange){
                             onChange(event.target.value)
                         }
-                    }}
-                    placeholder="Digite o SKU do produto" />
+                    }}/>
             </div>
         </div>
     )
--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -1,82 +1,71 @@
-import {useState } from 'react'
-import { Layout, Input} from 'components'
+import { useState } from 'react'
+import { Layout, Input } from 'components'
 
 export const CadastroProduto: React.FC = () => {
 
-  const [ sku, setSku] = useState<string>('')
-  const [ preco, setPreco ] = useState<string>('')
-  const [ nome, setNome] = useState<string>('')
-  const [ descricao, setDescricao] = useState<string>('')
+  const [sku, setSku] = useState<string>('')
+  const [preco, setPreco] = useState<string>('')
+  const [nome, setNome] = useState<string>('')
+  const [descricao, setDescricao] = useState<string>('')
 
-  const submit = () =>{
+  const submit = () => {
     const produto = {
       sku,
-      preco, 
-      nome, 
+      preco,
+      nome,
       descricao
     }
     console.log(produto)
   }
-    return (
-        <Layout titulo="Produtos">
-          <div className="columns">
-            <Input label="SKU: *"
-                   columnClasses="is-half"
-                   onChange={setSku}/>
-            <div className="field is-half column">
-              <label className="label" htmlFor="inputSku">SKU: *</label>
-                <div className="control">
-                   <input className="input"
-                     id="inputSku" value={sku}
-                     onChange={ event => setSku(event.target.value)}
-                     placeholder="Digite o SKU do produto"/>
-                 </div>
-            </div>
+  return (
+    <Layout titulo="Produtos">
+      <div className="columns">
+        <Input label="SKU: *"
+          columnClasses="is-half"
+          onChange={setSku}
+          value={sku}
+          id="inputSku"
+          placeholder="Digite o SKU do produto" />
 
-            <div className="field is-half column">
-              <label className="label" htmlFor="inputPreco">Preço: *</label>
-                <div className="control">
-                   <input className="input"
-                     id="inputPreco" value={preco}
-                     onChange={ event => setPreco(event.target.value)}
-                     placeholder="Digite o Preço do produto"/>
-                 </div>
-            </div>
-           </div>
+        <Input label="Preço: *"
+          columnClasses="is-half"
+          onChange={setPreco}
+          value={preco}
+          id="inputPreco"
+          placeholder="Digite o Preço do produto" />
+      </div>
 
-           <div className="columns">
-           <div className="field column is-full">
-              <label className="label" htmlFor="inputNome">Nome: *</label>
-                <div className="control">
-                   <input className="input" value={nome}
-                     id="inputNome"
-                     onChange={ event => setNome(event.target.value)}
-                     placeholder="Digite o Nome do produto"/>
-                 </div>
-           </div>
-           </div>
+      <div className="columns">
+      <Input label="Nome: *"
+          columnClasses="is-half"
+          onChange={setNome}
+          value={nome}
+          id="inputNome"
+          placeholder="Digite o Nome do produto" />
+       
+      </div>
 
-           <div className="columns">
-           <div className="field column is-full">
-              <label className="label" htmlFor="inputDesc">Descrição: *</label>
-                <div className="control">
-                   <input className="textarea"
-                     id="inputDesc" value={descricao}
-                     onChange={ event => setDescricao(event.target.value)}
-                     placeholder="Digite a descrição do produto"/>
-                 </div>
-           </div>
-           </div>
+      <div className="columns">
+        <div className="field column is-full">
+          <label className="label" htmlFor="inputDesc">Descrição: *</label>
+          <div className="control">
+            <input className="textarea"
+              id="inputDesc" value={descricao}
+              onChange={event => setDescricao(event.target.value)}
+              placeholder="Digite a descrição do produto" />
+          </div>
+        </div>
+      </div>
 
-           <div className="field is-grouped">
-            <div className="control">
-              <button onClick={submit} className="button is-link">Salvar</button>
-            </div>
-            <div className="control">
-              <button  className="button is-link is-light">Voltar</button>
-            </div>
-           </div>
-           
-        </Layout>
-    )
+      <div className="field is-grouped">
+        <div className="control">
+          <button onClick={submit} className="button is-link">Salvar</button>
+        </div>
+        <div className="control">
+          <button className="button is-link is-light">Voltar</button>
+        </div>
+      </div>
+
+    </Layout>
+  )
 }
